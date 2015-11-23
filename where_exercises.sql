34e8b4eabde89a4dbf3694bab181cba414e36473
ROGER CHIN <Roger@ROGERs-MacBook-Pro.local>
2015-11-23 13:25:45 -0600

    SQL exercises

---
+++ where_exercises.sql
@@ -0,0 +1,23 @@
+USE employees_db;
+
+ELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT emp_no, first_name
+FROM employees
+WHERE first_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+
